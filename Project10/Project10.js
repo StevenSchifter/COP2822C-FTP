f3397b0456f8bdaf5581bba005649ace28aaf00e
Steven Schifter <70248147+StevenSchifter@users.noreply.github.com>
2023-07-13 21:15:39 -0400

    Project 10, finished!
    CSS tweaked
    HTML refined
    JS logic fixed and commented

--- Project10/Project10.js
+++ Project10/Project10.js
@@ -1,39 +1,47 @@
+// Process the contents of the form fields
 function ProcessInput()
 {
-    // Get the value of Number X
+    // Get the value of Number X and assign it to the global variable X
     var X = document.forms["NumberForm"]["NumberX"].value;
+    // Make sure X is a number
+    X = parseFloat(X);
       
     // Check if Number X has a numeric value
-    // if(!isNaN(parseInt(X)))
-    // {
-        // alert("Please enter a numeric value for Number X.");
-        // return;
-    // }
+    if(isNaN(X))
+    {
+        alert("Please enter a numeric value for Number X.");
+        return;
+    }
     
-    // Get the value of Number Y
+    // Get the value of Number Y and assign it to the global variable Y
     var Y = document.forms["NumberForm"]["NumberY"].value;
+    // Make sure Y is a number
+    Y = parseFloat(Y);
 
     // Check if Number Y has a numeric value
-    // if(!isNaN(parseFloat(Y)))
-    // {
-        // alert("Please enter a numeric value for Number Y.");
-        // return;
-    // }
+    if(isNaN(Y))
+    {
+        alert("Please enter a numeric value for Number Y.");
+        return;
+    }
 
     // At this point, both numbers should have valid values, so they can be processed
     PresentResults(X, Y);
 }
 
+// Return the sum of X and Y
 function GetSum(X, Y)
 {
     return X + Y;
 }
 
+// Return the product of X and Y
 function GetProduct(X, Y)
 {
     return X * Y;
 }
 
+// Return a phrase indicating which number is bigger or if the numbers are equal
 function GetBigger(X, Y)
 {
     if (X > Y)
@@ -50,20 +58,21 @@ function GetBigger(X, Y)
     }
 }
 
+// Present the results in <p> elements below the button
 function PresentResults(X, Y)
 {
-    // Declare variables for the <p> elements where results will be presented
-    var SumElement     = document.getElementById("sum");
-    var ProductElement = document.getElementById("product");
-    var BiggerElement  = document.getElementById("bigger");
+    // Declare constants for the <p> elements
+    const SumElement     = document.getElementById("Sum");
+    const ProductElement = document.getElementById("Product");
+    const BiggerElement  = document.getElementById("Bigger");
     
-    // Declare variables for the function results
-    var SumResult     = GetSum(X, Y);
-    var ProductResult = GetProduct(X, Y);
-    var BiggerResult  = GetBigger(X, Y);
+    // Declare local variables for the function results
+    let SumResult     = GetSum(X, Y);
+    let ProductResult = GetProduct(X, Y);
+    let BiggerResult  = GetBigger(X, Y);
     
     // Change the values of the text in the <p> elements
-    SumElement.innerText = "The sum of " + X + " and " + Y + " is " + SumResult + "."
-    ProductElement.innerText = "The sum of " + X + " and " + Y + " is " + ProductResult + "."
-    BiggerElement.innerText = BiggerResult + "."
+    SumElement.innerText = "The sum of " + X + " and " + Y + " is " + SumResult + ".";
+    ProductElement.innerText = "The product of " + X + " and " + Y + " is " + ProductResult + ".";
+    BiggerElement.innerText = BiggerResult + ".";
 }
