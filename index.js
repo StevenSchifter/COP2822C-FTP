ca166824e9bd77b03c8d585d29ee9f11f046aa08
Steven Schifter <70248147+StevenSchifter@users.noreply.github.com>
2025-08-24 11:29:47 -0400

    Ported scheme selection JS from portfolio

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict";
 // Global constants
 const colorScheme = document.querySelector('meta[name="color-scheme"]');
+const schemeSelector = document.querySelector('#scheme-selector');
 
 // Detect color scheme preference or OS light/dark mode setting
 function detectColorSchemePref()
 {
-    let schemeSelector = document.querySelector('#scheme-selector');
     let usingDarkScheme = localStorage.getItem('usingDarkScheme');
     
     // First check local storage for dark scheme preference,
@@ -13,17 +13,14 @@ function detectColorSchemePref()
     if (usingDarkScheme === 'true')
     {
         toggleScheme('dark');
-        schemeSelector.value = 'dark';
     }
     else if (usingDarkScheme === 'false')
     {
         toggleScheme('light');
-        schemeSelector.value = 'light';
     }
     else // Default to automatic color scheme
     {
         toggleScheme('auto');
-        schemeSelector.value = 'auto';
     }
 }
 
@@ -32,19 +29,22 @@ function toggleScheme(schemePref)
 {
     // Change the content of the meta tag named "color-scheme",
     // and then set the color scheme preference in local storage
-    if (schemePref === 'light')
-    {
-        colorScheme.setAttribute('content', 'light');
-        localStorage.setItem('usingDarkScheme', 'false');
-    }
-    else if (schemePref === 'dark')
+    if (schemePref === 'dark')
     {
         colorScheme.setAttribute('content', 'dark');
         localStorage.setItem('usingDarkScheme', 'true');
+        schemeSelector.value = 'dark';
+    }
+    else if (schemePref === 'light')
+    {
+        colorScheme.setAttribute('content', 'light');
+        localStorage.setItem('usingDarkScheme', 'false');
+        schemeSelector.value = 'light';
     }
     else if (schemePref === 'auto')
     {
         colorScheme.setAttribute('content', 'light dark');
         localStorage.setItem('usingDarkScheme', 'auto');
+        schemeSelector.value = 'auto';
     }
 }
